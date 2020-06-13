49e4e112635f7096ed208b0cb722da862c20b2b0
Megadardery <Megadardery@yahoo.com>
2020-06-13 21:52:38 +0200

    playing around with CSS and basic React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactList extends Component {
+    render() {
+        const people = this.props.contacts;
+        return (<ol>
+            {people.map((v, index) => <li key={index}>{v.name}</li>)}
+        </ol>);
+    }
+}
+
+class Header extends Component {
+    render() {
+        return <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo"/>
+            <p>
+                Welcome to my React Course Testing facility!
+            </p>
+        </header>
+    }
+}
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header/>
+                <ContactList contacts={[
+                    {name: "ahmed"},
+                    {name: "ibrahim"},
+                    {name: "hossam"}
+                ]}/>
+                <ContactList contacts={[
+                    {name: "nooo"},
+                    {name: "ibrahim"},
+                    {name: "hossam"}
+                ]}/>
+            </div>
+        );
+    }
 }
 
 export default App;
